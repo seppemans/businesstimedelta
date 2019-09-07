d99e52aa09a276ca9d502604f7f08a19e0a413d9
seppemans <sep@seplog.net>
2019-09-07 09:26:35 -0700

    add python 3.6 and 3.7 to CI

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business :: Scheduling'
     ],
     keywords='business working time timedelta hours businesstime businesshours',
